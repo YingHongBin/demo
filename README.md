051620f53ae515e6702ad679860caa154f65af36
hongbin <ying.hongbin.1016@gmail.com>
2024-11-19 11:13:32 +0800

    feat: Initial project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Demo Project
+This repo aim to build a demo spring project with java 21 for learning purpose. 
---
+++ src/main/java/org/hongbin/controller/DemoController.java
@@ -0,0 +1,16 @@
+package org.hongbin.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        log.info("Hello, World!");
+        return "Hello, World!";
+    }
+}
